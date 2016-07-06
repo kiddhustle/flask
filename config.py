09433d74e01f4297361d72fae1a66dbe11b837dc
kiddhustle <OnlineDevTemp@condenast.co.uk>
2016-07-06 16:24:27 +0100

    app restructure

---
+++ config.py
@@ -0,0 +1,44 @@
+import os
+from configparser import ConfigParser
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+config = ConfigParser()
+config.read(basedir + '/configs/app.ini')
+
+class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY') or  's3cr3tk3y'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = config['email']['FLASKY_ADMIN']
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = config['email']['MAIL_USERNAME']
+    MAIL_PASSWORD = config['email']['MAIL_PASSWORD']
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
