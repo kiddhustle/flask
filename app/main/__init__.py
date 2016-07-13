dabcb1ef2065c1c7b32e70e478a3b01c8a96cffa
kiddhustle <OnlineDevTemp@condenast.co.uk>
2016-07-13 16:58:19 +0100

    permissions added

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,3 +4,9 @@ main = Blueprint('main', __name__)
 pink = 'blue'
 
 from . import views, errors
+from ..models import Permission
+
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
